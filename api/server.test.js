981175a89182e02a67f28ee6f7d135d216dee9ee
techwulf <t.zach.moore@gmail.com>
2021-10-15 13:29:19 -0600

    setup testfile for making tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,26 @@
-// Write your tests here
+const request = require('supertest');
+
+const server = require('./server');
+const db = require('../data/dbConfig');
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true);
+});
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+
+describe('[POST] /api/auth', () => {
+  let res;
+  const newUser = {username: 'legolas', password: 'thatOnlyCountsAsOne'};
+  beforeEach(async () => {
+    res = await request(server).post('/api/auth/')
+      .send(newUser);
+  });
 })
